de3512cda633a52a1a7b9951f519c5e248e9c552
Chandupa2001 <chanduparanawaka2001@gmail.com>
2024-07-08 13:51:10 +0530

    Completed frontend

--- food-delivery/frontend/src/components/Navbar/Navbar.jsx
+++ food-delivery/frontend/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { FaSearch, FaCartPlus } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
 
 const Navbar = ({setShowLogin}) => {
 
     const [menu, SetMenu] = useState("home");
+    const {getTotalCartAmount} = useContext(StoreContext);
 
   return (
     <div className='navbar'>
-        <img src={assets.logo} alt="" className="logo" />
+        <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
         <ul className="navbar-menu">
             <Link to='/' onClick={()=>SetMenu("home")} className={menu==="home"?"active":""}>Home</Link>
             <a href='#explore-menu' onClick={()=>SetMenu("menu")} className={menu==="menu"?"active":""}>Menu</a>
@@ -19,8 +21,8 @@ const Navbar = ({setShowLogin}) => {
         <div className="navbar-right">
             <FaSearch size={20} color='#49557e'/>
             <div className="navbar-search-icon">
-                <FaCartPlus size={20} color='#49557e'/>
-                <div className="dot"></div>
+                <Link to={'/cart'}><FaCartPlus size={20} color='#49557e'/></Link>
+                <div className={getTotalCartAmount()===0?"":"dot"}></div>
             </div>
             <button onClick={()=>setShowLogin(true)} >Sign In</button>
         </div>
--- food-delivery/frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ food-delivery/frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
-import './PlaceOrder.css'
+import React, { useContext } from "react";
+import "./PlaceOrder.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const PlaceOrder = () => {
-  return (
-    <div>PlaceOrder</div>
-  )
-}
 
-export default PlaceOrder
+  const {getTotalCartAmount} = useContext(StoreContext);
+
+  return (
+    <form className="place-order" action="">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <input type="email" placeholder="Email address" />
+        <input type="text" placeholder="Street" />
+        <div className="multi-fields">
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="State" />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder="Zip code" />
+          <input type="text" placeholder="Country" />
+        </div>
+        <input type="text" placeholder="Phone" />
+      </div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+          <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>Rs. {getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>Rs. {getTotalCartAmount()===0?0:2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>Rs. {getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+            </div>
+          </div>
+          <button>PROCEED TO PAYMENT</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default PlaceOrder;
