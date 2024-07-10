e67ab4db0b46266e3438b9b008684b33ae7571cd
Chandupa2001 <chanduparanawaka2001@gmail.com>
2024-07-10 20:01:02 +0530

    Created admin panel with frontend and backend

---
+++ food-delivery/admin/src/assets/assets.js
@@ -0,0 +1,15 @@
+import logo from './logo.png'
+import add_icon from './add_icon.jpg'
+import order_icon from './order_icon.jpg'
+import parcel_icon from './parcel_icon.png'
+import profile_image from './profile_image.jpg'
+import upload_area from './upload_area.png'
+
+export const assets = {
+    logo,
+    add_icon,
+    order_icon,
+    profile_image,
+    upload_area,
+    parcel_icon
+}
--- food-delivery/backend/controllers/foodController.js
+++ food-delivery/backend/controllers/foodController.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 // add food item
 const addFood = async (req,res) => {
 
-    let image_filename = `${req.file.filename}`
+    let image_filename = `${req.file.filename}`;
 
     const food = new foodModel({
         name: req.body.name,
